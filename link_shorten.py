882e3e290389add62a1a2c7ee465a7c9791f16d4
mpracu <manugvd@gmail.com>
2022-05-28 01:55:33 +0200

    Main code

---
+++ link_shorten.py
@@ -0,0 +1,8 @@
+import pyshorteners
+
+link= "https://www.amazon.de/dp/B084DWG2VQ/ref=gw_de_desk_mso_aucc_brw_bau_xpl2?pf_rd_r=HTCGZEVRYDN8T0T6D742&pf_rd_p=5da74227-0533-497a-bc36-f9385b8377cb&pd_rd_r=81ed1ddb-9f89-444f-b6e4-2c053b232d1e&pd_rd_w=KhnL5&pd_rd_wg=Ji4P8&ref_=pd_gw_unk"
+
+short = pyshorteners.Shortener()
+short_link = short.tinyurl.short(link)
+
+print("New link : "+short_link)
